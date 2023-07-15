5f54a5e97ca9c494366e80bce0e6a652919681fe
Enea Dhack <enea.so@live.com>
2023-07-14 20:01:48 -0500

    configure tests

---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author enea dhack <enea.so@live.com>
+ */
+
+declare(strict_types=1);
+
+namespace Vaened\PriceEngine\Tests;
+
+use PHPUnit\Framework\TestCase as PhpUnitTestCase;
+
+abstract class TestCase extends PhpUnitTestCase
+{
+}
+
