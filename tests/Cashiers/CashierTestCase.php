aa1641818bd0b0bd6de6a87fc3086d7b0ff83bd3
Enea Dhack <enea.so@live.com>
2023-07-23 20:31:41 -0500

    refactor: improve tests

--- tests/Cashiers/CashierTestCase.php
+++ tests/Cashiers/CashierTestCase.php
@@ -14,7 +14,10 @@ use Vaened\PriceEngine\Cashier;
 use Vaened\PriceEngine\Tests\TestCase;
 use Vaened\PriceEngine\Tests\Utils\Summary;
 
+use function call_user_func;
+use function explode;
 use function Lambdish\Phunctional\each;
+use function Lambdish\Phunctional\reduce;
 use function sprintf;
 
 abstract class CashierTestCase extends TestCase
@@ -50,18 +53,38 @@ abstract class CashierTestCase extends TestCase
     protected function assertTotals(Summary $summary): void
     {
         $this->assertQuantity($summary->quantity());
-        $this->assertUnitPrice($summary->unitPrice());
-        $this->assertSubtotal($summary->subtotal());
-        $this->assertTotalTaxes($summary->totalTaxes());
-        $this->assertTotalCharges($summary->totalCharges());
-        $this->assertTotaDiscounts($summary->totalDiscounts());
-        $this->assertTotal($summary->total());
+        $this->assertTotal('unitPrice', $summary->unitPrice());
+        $this->assertTotal('subtotal', $summary->subtotal());
+        $this->assertTotal('taxes->total', $summary->totalTaxes());
+        $this->assertTotal('charges->total', $summary->totalCharges());
+        $this->assertTotal('discounts->total', $summary->totalDiscounts());
+        $this->assertTotal('total', $summary->total());
     }
 
     protected function setUp(): void
     {
         parent::setUp();
         $this->cashier = $this->cashier();
+    }
+
+    private function assertTotal(string $label, Money $expected): void
+    {
+        $total = reduce(
+            static fn(object $context, string $method) => call_user_func([$context, $method]),
+            explode('->', $label),
+            $this->cashier
+        );
+
+        $this->assertEquals(
+            $expected,
+            $total,
+            sprintf(
+                'Failed asserting that the result <%s> of Cart::%s() is equal to <%s>.',
+                $total->getAmount(),
+                $label,
+                $expected->getAmount()
+            )
+        );
     }
 
     private function assertAdjustmentEquals(Adjustments $expected): callable
@@ -72,80 +95,5 @@ abstract class CashierTestCase extends TestCase
     private function assertQuantity(int $quantity): void
     {
         $this->assertEquals($this->cashier->quantity(), $quantity);
-    }
-
-    private function assertUnitPrice(Money $unitPrice): void
-    {
-        $this->assertEquals($this->cashier->unitPrice(),
-            $unitPrice,
-            sprintf(
-                'Failed asserting that the unit price of <%s> equals <%s>.',
-                $unitPrice->getAmount(),
-                $this->cashier->unitPrice()->getAmount()
-            )
-        );
-    }
-
-    private function assertSubtotal(Money $subtotal): void
-    {
-        $this->assertEquals($this->cashier->subtotal(),
-            $subtotal,
-            sprintf(
-                'Failed asserting that the subtotal of <%s> equals <%s>.',
-                $subtotal->getAmount(),
-                $this->cashier->subtotal()->getAmount()
-            ));
-    }
-
-    private function assertTotalTaxes(Money $totalTaxes): void
-    {
-        $total = $this->cashier->taxes()->total();
-
-        $this->assertEquals(
-            $total,
-            $totalTaxes,
-            sprintf(
-                'Failed asserting that the total taxes of <%s> equals <%s>.',
-                $totalTaxes->getAmount(),
-                $total
-            ));
-    }
-
-    private function assertTotalCharges(Money $totalCharges): void
-    {
-        $total = $this->cashier->charges()->total();
-
-        $this->assertEquals(
-            $total,
-            $totalCharges,
-            sprintf(
-                'Failed asserting that the total charges of <%s> equals <%s>.',
-                $totalCharges->getAmount(),
-                $total->getAmount()
-            ));
-    }
-
-    private function assertTotaDiscounts(Money $totaDiscounts): void
-    {
-        $total = $this->cashier->discounts()->total();
-
-        $this->assertEquals(
-            $total,
-            $totaDiscounts,
-            sprintf(
-                'Failed asserting that the tota discounts of <%s> equals <%s>.',
-                $totaDiscounts->getAmount(),
-                $total->getAmount()));
-    }
-
-    private function assertTotal(Money $total): void
-    {
-        $this->assertEquals($this->cashier->total(),
-            $total,
-            sprintf(
-                'Failed asserting that the definitive total of <%s> equals <%s>.',
-                $total->getAmount(),
-                $this->cashier->total()->getAmount()
-            ));
     }
 }
