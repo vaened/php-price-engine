f649316bbf03b0408d475e9f0d6cf929a441b5fd
Enea Dhack <enea.so@live.com>
2023-07-28 18:30:30 -0500

    docs: simple cashier

--- src/Cashiers/SimpleCashier.php
+++ src/Cashiers/SimpleCashier.php
@@ -10,9 +10,15 @@ namespace Vaened\PriceEngine\Cashiers;
 use Brick\Money\Money;
 use Vaened\PriceEngine\Adjustments\Adjusters;
 use Vaened\PriceEngine\Cashier;
-use Vaened\PriceEngine\Money\Prices\Price;
 use Vaened\PriceEngine\Money\Prices\FlatPrice;
+use Vaened\PriceEngine\Money\Prices\Price;
 
+/**
+ * The SimpleCashier class represents a cashier that calculates prices based on the gross price without taxes.
+ *
+ * This cashier applies discounts and charges directly to the base price before adding taxes, resulting in a
+ * simple and straightforward price calculation.
+ */
 final class SimpleCashier extends Cashier
 {
     protected function createUnitPrice(Money $grossUnitPrice, Adjusters $taxes): Price
