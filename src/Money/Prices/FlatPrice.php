e6b57e045b31edc9ee4cd01c5331da224466444c
Enea Dhack <enea.so@live.com>
2023-07-31 12:25:30 -0500

    refactor: improve price

--- src/Money/Prices/FlatPrice.php
+++ src/Money/Prices/FlatPrice.php
@@ -7,24 +7,16 @@ declare(strict_types=1);
 
 namespace Vaened\PriceEngine\Money\Prices;
 
-use Brick\Money\Context;
-use Brick\Money\Currency;
 use Brick\Money\Money;
 
 final class FlatPrice implements Price
 {
-    private readonly Currency $currency;
-
-    private readonly Context  $context;
-
     public function __construct(
         private readonly Money $discountable,
         private readonly Money $chargeable,
         private readonly Money $taxable,
     )
     {
-        $this->currency = $taxable->getCurrency();
-        $this->context  = $taxable->getContext();
     }
 
     public function discountable(): Money
@@ -40,15 +32,5 @@ final class FlatPrice implements Price
     public function taxable(): Money
     {
         return $this->taxable;
-    }
-
-    public function currency(): Currency
-    {
-        return $this->currency;
-    }
-
-    public function context(): Context
-    {
-        return $this->context;
     }
 }
--- src/Money/Prices/Price.php
+++ src/Money/Prices/Price.php
@@ -7,8 +7,6 @@ declare(strict_types=1);
 
 namespace Vaened\PriceEngine\Money\Prices;
 
-use Brick\Money\Context;
-use Brick\Money\Currency;
 use Brick\Money\Money;
 
 interface Price
@@ -18,8 +16,4 @@ interface Price
     public function taxable(): Money;
 
     public function chargeable(): Money;
-
-    public function currency(): Currency;
-
-    public function context(): Context;
 }
